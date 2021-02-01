05553c51a0549b4be5e4b4095161a210a41144bb
Jerry Wester <JerryWester@users.noreply.github.com>
2021-01-31 20:00:43 -0700

    Initial commit

---
+++ src/OoTTextReplace/TextReplacement.ts
@@ -0,0 +1,45 @@
+import { EventHandler } from 'modloader64_api/EventHandler';
+import { IModLoaderAPI, IPlugin, ModLoaderEvents } from 'modloader64_api/IModLoaderAPI';
+import { Z64LibSupportedGames } from 'Z64Lib/API/Z64LibSupportedGames';
+import { Z64RomTools } from 'Z64Lib/API/Z64RomTools';
+import fs from 'fs';
+import path from 'path';
+
+class TextReplacement implements IPlugin {
+
+    ModLoader!: IModLoaderAPI;
+
+    preinit() {}
+    init() {}
+    postinit() {}
+    onTick() {}
+
+    @EventHandler(ModLoaderEvents.ON_ROM_PATCHED)
+    onRomPatched(evt: any) {
+        let rom = evt.rom as Buffer;
+        let tools = new Z64RomTools(this.ModLoader, Z64LibSupportedGames.OCARINA_OF_TIME);
+        if (!!((this as any)['metadata']['EN_message'] as string)) {
+            let file = fs.readFileSync(path.resolve(__dirname, (this as any)['metadata']['EN_message'] as string));
+            if (tools.recompressDMAFileIntoRom(rom, 22, file)){
+                this.ModLoader.logger.info("Successfully replaced nes_message_data_static");
+            } else {
+                tools.noCRC(rom);
+                let address = tools.relocateFileToExtendedRom(rom, 22, file);
+                this.ModLoader.logger.info(`Failed to replace nes_message_data_static, relocating to 0x${address.toString(16).toUpperCase()}`);
+            }
+        }
+        if (!!((this as any)['metadata']['JP_message'] as string)) {
+            let file = fs.readFileSync(path.resolve(__dirname, (this as any)['metadata']['JP_message'] as string));
+            if (tools.recompressDMAFileIntoRom(rom, 19, file)){
+                this.ModLoader.logger.info("Successfully replaced jpn_message_data_static");
+            } else {
+                tools.noCRC(rom);
+                let address = tools.relocateFileToExtendedRom(rom, 19, file);
+                this.ModLoader.logger.info(`Failed to replace jpn_message_data_static, relocating to 0x${address.toString(16).toUpperCase()}`);
+            }
+        }
+    }
+
+}
+
+module.exports = TextReplacement;
